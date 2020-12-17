7a04d5a3e5f70b243b0ee666dbcbcb88d752b37f
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-12-17 09:24:57 -0600

    initial commit

---
+++ Storytime Writer's Assistant/Shared/Storytime_Writer_s_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Storytime_Writer_s_AssistantApp.swift
+//  Shared
+//
+//  Created by Ellie Lockhart on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Storytime_Writer_s_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
